a856cfeaa929783148a2aa887d0ecef80a560476
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-07-31 15:32:22 +0100

    MAINT: Maintain configuration files

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", 'r') as readme:
+    long_description = readme.read()
 
 with open("requirements.txt", 'r') as dependencies:
     requirements = [pkg.strip() for pkg in dependencies]
 
 setuptools.setup(
     name="matryoshka",
-    version="0.2.16",
+    version="0.2.17.dev0",
     author="Jamie Donald-McCann",
     author_email="jamie.donald-mccann@port.ac.uk",
     description="Emulated halo model",
@@ -20,10 +20,10 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
     ],
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
